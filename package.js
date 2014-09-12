272920ea32d090a5103fadaf49b8048bf73d2def
richsilv@yahoo.co.uk <richardlsilverton32@googlemail.com>
2014-09-12 15:04:59 +0100

    minor amendments

--- package.js
+++ package.js
@@ -1,18 +1,25 @@
 Package.describe({
-  summary: "A simple multi-page Tour/Joyride plugin designed to play nicely with Iron Router"
+ 	summary: "A simple multi-page Tour/Joyride plugin designed to play nicely with Iron Router",
+	version: "1.0.0",
+	git: "https://github.com/richsilv/iron-router-tour.git"
 });
 
-Package.on_use(function (api, where) {
-  api.use(['iron-router', 'templating', 'underscore', 'jquery'], 'client');
-  api.use("scss", ['client', 'server']);
-  api.imply('scss', ['client', 'server']);
-  
-  api.add_files('iron-router-tour.js', 'client');
-  api.add_files('iron-router-tour.html', 'client');
+Package.onUse(function (api, where) {
+	api.versionsFrom('METEOR@0.9.1');
+	api.use(['templating', 'underscore', 'jquery'], 'client');
+	api.use('iron:router@0.9.1', ['client', 'server']);
+	api.imply('iron:router@0.9.1', ['client', 'server']);
+	// api.use('iron-router', ['client', 'server']);
+	// api.imply('iron-router', ['client', 'server']);
+	api.use("fourseven:scss@0.9.5", ['client', 'server']);
+  	api.imply('fourseven:scss@0.9.5', ['client', 'server']);
+
+  	api.addFiles('iron-router-tour.js', 'client');
+  	api.addFiles('iron-router-tour.html', 'client');
+
 });
 
 Package.on_test(function (api) {
-  api.use('iron-router-tour');
-
-  api.add_files('iron-router-tour_tests.js', 'client');
+	api.use('richsilv:iron-router-tour');
+	api.add_files('iron-router-tour_tests.js', 'client');
 });
