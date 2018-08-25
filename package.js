80f9194ef2ced7597a0fd6ab51f5ce204840fb57
carrot93 <carrot.live@hotmail.com>
2018-08-26 04:04:33 +0800

    support meteor 1.7

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
 Package.describe({
  	summary: "A simple multi-page Tour/Joyride plugin designed to play nicely with Iron Router",
-	version: "1.0.3_1",
+	version: "1.0.3_2",
 	git: "https://github.com/carrot93/iron-router-tour.git"
 });
 
 Package.onUse(function (api, where) {
 	api.versionsFrom('METEOR@0.9.1');
 	api.use(['templating', 'underscore', 'jquery'], 'client');
-	api.use('iron:router@1.0.0', ['client', 'server']);
-	api.imply('iron:router@1.0.0', ['client', 'server']);
+	api.use('iron:router', ['client', 'server']);
+	api.imply('iron:router', ['client', 'server']);
 	// api.use('iron-router', ['client', 'server']);
 	// api.imply('iron-router', ['client', 'server']);
-	api.use("fourseven:scss@0.9.5", ['client', 'server']);
-  	api.imply('fourseven:scss@0.9.5', ['client', 'server']);
+	api.use("fourseven:scss", ['client', 'server']);
+  	api.imply('fourseven:scss', ['client', 'server']);
 
   	api.addFiles('iron-router-tour.js', 'client');
   	api.addFiles('iron-router-tour.html', 'client');
